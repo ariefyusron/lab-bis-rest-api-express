d2b830b993142b6028a199706bcbc8377be85ad6
Arief Yusron <ariefyusron98@gmail.com>
2019-01-23 01:25:54 +0700

    create endpoint login

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,12 +1,36 @@
+const bcrypt = require('bcrypt')
+
 const models = require('../models')
 
 exports.register = async (req,res) => {
   try{
     const storeUser = await models.Users.create(req.body)
     res.json(storeUser)
-  } catch(error){
+  } catch{
     res.status(400).json({
       message: 'NIM is already'
+    })
+  }
+}
+
+exports.login = async (req,res) => {
+  const showUser = await models.Users.findOne({
+                      where: {
+                        nim: req.body.nim
+                      }
+                    })
+  if(showUser){
+    const compare = bcrypt.compareSync(req.body.password || '', showUser.password)
+    if(compare){
+      res.json(showUser)
+    } else{
+      res.status(400).json({
+        message: 'invalid Password'
+      })
+    }
+  } else{
+    res.status(400).json({
+      message: 'NIM is not available'
     })
   }
 }
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/auth')
 const middleware = require('../middlewares/auth')
 
 router.post('/register',middleware.register,controller.register)
+router.post('/login',controller.login)
 
 module.exports = router
