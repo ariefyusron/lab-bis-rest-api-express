2b5cdf3a1345c417fe9bf746a979929fbd867f38
Arief Yusron <ariefyusron98@gmail.com>
2019-01-23 04:15:13 +0700

    create endpoint show profile

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,5 +1,14 @@
 const models = require('../models')
 
+exports.show = async (req,res) => {
+  const showUser = await models.ProfileUser.findOne({
+    where: {
+      user_id: req.userData.id
+    }
+  })
+  res.json(showUser)
+}
+
 exports.update = async (req,res) => {
   await models.ProfileUser.update(
     req.body,{
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const controller = require('../controllers/profile')
 const middleware = require('../middlewares/auth')
 
+router.get('/profile',middleware.checkAuth,controller.show)
 router.patch('/profile',middleware.checkAuth,controller.update)
 
 module.exports = router
